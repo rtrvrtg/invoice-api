50797bbb1bfcc9d603bc36a350aebd20da433e9b
Rob Howard <rob@robhoward.id.au>
2013-02-02 21:28:09 +1100

    Adds a test environment to the config/environments.rb template.

--- tasks/templates/environments.rb
+++ tasks/templates/environments.rb
@@ -1,6 +1,6 @@
 #
 # Change these depending on your settings.
-# (This example setup assumes a SQLite dev and a Heroku production.)
+# (This example setup assumes a SQLite dev/test and a Heroku production.)
 #
 
 configure do
@@ -13,6 +13,13 @@ configure :development do
   set :database_url, "sqlite3://#{File.expand_path('../db/development.sqlite3', File.dirname(__FILE__))}"
 end
 
+configure :test do
+  require 'sqlite3'
+  set :database_url, "sqlite3://#{File.expand_path('../db/test.sqlite3', File.dirname(__FILE__))}"
+end
+
+# Not usable by Heroku (can't use files not checked in), but useful
+# if set up anywhere else.
 configure :production do
   set :database_url, ENV['DATABASE_URL']
 end
